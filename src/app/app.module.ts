423760a5f5da2407c526135f70b2b60a77396d3c
Andres Contreras <andres_c8@outlook.com>
2018-10-19 14:28:18 -0500

    routing terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -36,10 +37,12 @@ import { AppRoutingModule } from './app-routing.module';
     PrincipalBodComponent,
     PrincipalCajComponent,
     NavBodComponent,
+    
    
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,58 +8,17 @@ import {RestCLientService } from '../../services/rest-client.service';
 })
 export class LoginComponent implements OnInit {
 
-  user = 'user';
-  password = 'password';
+  user = '';
+  password = '';
   result: any ; 
   message: any;
-  constructor(private restClient: RestCLientService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  doLogin() {
-    console.log(this.user + ' - ' + this.password);
-    this.restClient.login(this.user, this.password).subscribe(data => {
-        this.message = 'Login Ok';
-      }, error => {
-        console.error(error);
-        this.message = JSON.stringify(error);
-      });
-  }
+  
 
-  getTestData() {
-    this.restClient.getTestData().subscribe(
-      data => {
-        console.log('Success' + data);
-        this.message = JSON.stringify(data);
-      },
-      error => {
-        console.error(error);
-        this.message = JSON.stringify(error);
-      }
-    );
-  }
-
-  getRestrictedData() {
-    this.restClient.getRestrictedData().subscribe(
-      data => {
-        console.log('Success' + data);
-        this.message = JSON.stringify(data);
-      },
-      error => {
-        console.error(error);
-        this.message = JSON.stringify(error);
-      }
-    );
-  }
-
-  logout() {
-    this.restClient.logout().subscribe(data => {
-        this.message = 'Logout Ok';
-      }, error => {
-        console.error(error);
-        this.message = JSON.stringify(error);
-      });
-  }
+  
 }
 
