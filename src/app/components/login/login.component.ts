cc207dcabdcdce94dfd6c672f1bd74b0b70f3637
Andres Contreras <andres_c8@outlook.com>
2018-10-19 15:51:04 -0500

    cambios compra y producto

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {RestCLientService } from '../../services/rest-client.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +13,24 @@ export class LoginComponent implements OnInit {
   password = '';
   result: any ; 
   message: any;
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
-  
+  gotoHeroes(n) {
+    console.log("ROUTER");
+    this.router.navigate(['/cajero']);
+    if(n == 1)
+    {
+      this.router.navigate(['/cajero']);
+    }
+    else if(n == 2)
+    {
+      this.router.navigate(['/bodeguero']);
+    }
+  }
 
   
 }
