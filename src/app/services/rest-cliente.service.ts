bee9457e3817cfdafc9a5e69244d5682628903c2
Andres Contreras <andres_c8@outlook.com>
2018-10-24 10:58:24 -0500

    cambios rest

--- src/app/services/rest-cliente.service.ts
+++ src/app/services/rest-cliente.service.ts
@@ -1,9 +1,25 @@
+import { Cliente } from './../class/cliente';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestClienteService {
 
-  constructor() { }
+  baseUrl = environment.baseUrl;
+  url = this.baseUrl + '/cliente';
+  constructor(private http: HttpClient) {
+  }
+
+  getAllData(): Observable<Cliente[]>{
+
+    return this.http.get<Cliente[]>(this.url);
+  }
+
+  getClientebyId(id: number): Observable<Cliente>{
+    return this.http.get<Cliente>(this.url);
+  }
 }
