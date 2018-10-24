68bd244cf6b5d5fcf64587c69cf3a152fd944d61
Andres Contreras <andres_c8@outlook.com>
2018-10-24 13:44:48 -0500

    login por rest

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 //import {RestCLientService } from '../../services/rest-client.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Usuario } from 'src/app/class/usuario';
+import { SecurityService } from 'src/app/services/security.service';
 
 @Component({
   selector: 'app-login',
@@ -14,10 +15,21 @@ export class LoginComponent implements OnInit {
   result: any ;
   message: any;
   constructor(private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private security: SecurityService) { }
 
   ngOnInit() {
     this.usuario = new Usuario();
+  }
+
+  doLogin() {
+    console.log(this.usuario.usuario,"--",this.usuario.contrasena);
+    this.security.login(this.usuario.usuario, this.usuario.contrasena).subscribe(data => {
+        this.message = 'Login Ok';
+      }, error => {
+        console.error(error);
+        this.message = JSON.stringify(error);
+      });
   }
 
   gotoHeroes(n) {
--- src/app/services/security.service.ts
+++ src/app/services/security.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SecurityService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    const params = new HttpParams()
+      .set('username', username)
+      .set('password', password);
+    return this.http.post('http://localhost:8080/login', null, {
+      headers: headers,
+      params: params,
+      withCredentials: true
+    });
+  }
+
 }
